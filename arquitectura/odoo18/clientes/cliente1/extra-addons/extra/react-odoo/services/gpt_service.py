4115b690fb76a23d086a1d6bbaecf2f821587f72
root <root@vmi2491426.contaboserver.net>
2025-09-01 19:29:02 +0200

    GPT Corrector ortografico

--- arquitectura/odoo18/clientes/cliente1/extra-addons/extra/react-odoo/services/gpt_service.py
+++ arquitectura/odoo18/clientes/cliente1/extra-addons/extra/react-odoo/services/gpt_service.py
@@ -15,14 +15,24 @@ class GptService(models.TransientModel):
     _name = 'gpt.service'
     _description = 'GPT Service Layer'
     
+    @api.model
+    def _get_openai_client(self):
+        api_key = self.env['openai.config'].sudo().search([('active', '=', True)], limit=1).api_key
+        if not api_key:
+            None
+            raise ValidationError(_('Configura la clave de API de OpenAI en Ajustes.'))
+        return OpenAI(api_key=api_key)
     
     @api.model
     def orthography_check(self, prompt, max_tokens=None):
         """Verificación ortográfica usando el caso de uso"""
+        openai_client = self._get_openai_client()
         use_case = self.env['orthography.use.case']
-        options = {"prompt": prompt}
-        if max_tokens:
-            options["max_tokens"] = max_tokens
+        options = {"prompt": prompt,
+                   "max_tokens": max_tokens,
+                   "openai_client": openai_client,
+                   }
+         
         # Implementa aquí la lógica real de verificación ortográfica
-        # Por ahora devolvemos un ejemplo básico
+        # Por ahora devolvemos un ejemplo básaico
         return use_case.execute(options)
