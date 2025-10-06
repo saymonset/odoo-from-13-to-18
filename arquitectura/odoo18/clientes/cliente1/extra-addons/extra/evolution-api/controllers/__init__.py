d7a7673526cbc188b13e2279a301df4be9c4c593
saymonset <saymon_set@hotmail.com>
2025-10-07 01:32:38 +0200

    Procesando recibir me nsajes

--- arquitectura/odoo18/clientes/cliente1/extra-addons/extra/evolution-api/controllers/__init__.py
+++ arquitectura/odoo18/clientes/cliente1/extra-addons/extra/evolution-api/controllers/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-from . import main
 from . import recieve_message_controller
+from . import procesar_message_controller
--- arquitectura/odoo18/clientes/cliente1/extra-addons/extra/evolution-api/controllers/procesar_message_controller.py
+++ arquitectura/odoo18/clientes/cliente1/extra-addons/extra/evolution-api/controllers/procesar_message_controller.py
@@ -7,47 +7,32 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-class WhatsAppWebhook(http.Controller):
-    #https://n8n.jumpjibe.com/webhook/webhook0
-    #https://jumpjibe.com/webhook/n8n-messages-upsert
-    #@http.route('/webhook/n8n-messages-upsert', type='http', auth='public', methods=['POST'], csrf=False)
-    @http.route('/webhook/n8n-messages-upsert', type='http', auth='public', methods=['POST'], csrf=False)
+class ProcesarMessageController(http.Controller):
+    @http.route('/webhook/procesar-messages', type='http', auth='public', methods=['POST'], csrf=False)
     def handle_n8n_messages_upsert(self, **kw):
         # Intenta obtener el JSON del cuerpo
         try:
             data = json.loads(request.httprequest.data.decode('utf-8'))
             _logger.info("data: %s", str(data))
-            service = request.env['evolution.service'].sudo()
+            service = request.env['procesar_mensaje.service'].sudo()
             #Informacion de evolution api ya procesada la info
             info = service.getInfo(data)
-           # serviceIA = http.request.env['gpt.service']
-          #  result = serviceIA.orthography_check('ola mundo', max_tokens=100)
-        
-            
-            
-            # Buscamos por cliente el último registro para obtener el threadid  < a 48 horas, si no creamos uno nuevo
-            service_info_whats_app = request.env['model_info_whats_app.service'].sudo()
-           
-           # Si el threadId esta vacio, lo generara la IA
-            threadId = service_info_whats_app.getLastRecordInfo(info)
-            info['thread_id'] = threadId
-            
-            # LLamamos capa service IA para crear la respuesta de la IA
-            iaService = request.env['ia.service'].sudo()
-            info = iaService.generateThreadIdAndAnswerIA(info)
-            
-            # Guardamos el threadid si es nuevo y la respuesta de la IA
-            service_info_whats_app = request.env['model_info_whats_app.service'].sudo()
-            info = service_info_whats_app.getSaveData(info)
-            
             
             
             # Ahora la URL usa tu instancia "odoosaymon"
             evolution_url = f"https://evolution.jumpjibe.com/message/sendText/{info.get('instance')}"
             payload = {
             "number": info.get('client_phone'),
-            "text": info.get('conversation_ia') 
+             "text": (
+                        f"host_phone: {info.get('host_phone')}\n"
+                        f"instance: {info.get('instance')}\n"
+                        f"message_type: {info.get('message_type')}\n"
+                        f"conversation: {info.get('conversation')}"
+                    )
          }
+            _logger.info("payload: %s", str(payload))
+            _logger.info("payload: %s", str(payload))
+            
             try:
                 headers = {'Content-Type': 'application/json', 'apikey': info.get('apikey')}
                 resp = requests.post(evolution_url, json=payload, headers=headers, timeout=10)
