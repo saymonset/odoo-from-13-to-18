841e3edaa7c1fafd16b6d388748376e246a5f21a
saymonset <saymon_set@hotmail.com>
2025-10-14 00:40:05 +0200

    QUERY:Productos más vendidos con margen y categoría

--- arquitectura/odoo18/clientes/cliente1/extra-addons/extra/business_intelligence_queries/controllers/__init__.py
+++ arquitectura/odoo18/clientes/cliente1/extra-addons/extra/business_intelligence_queries/controllers/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 from . import product_controller
 from . import sale_order_line_details_controller
-from . import sales_custom_queries_controller
+from . import sale_order_line_details_controller
+from . import products_more_sells_queries_controller
