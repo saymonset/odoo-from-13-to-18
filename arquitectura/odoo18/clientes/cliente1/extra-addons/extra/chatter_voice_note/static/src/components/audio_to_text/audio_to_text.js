f940f1b6114e167122c229d81881632fd3235dfe
saymonset <saymon_set@hotmail.com>
2025-10-30 14:59:04 +0100

    Se embebe el componente

--- arquitectura/odoo18/clientes/cliente1/extra-addons/extra/chatter_voice_note/static/src/components/audio_to_text/audio_to_text.js
+++ arquitectura/odoo18/clientes/cliente1/extra-addons/extra/chatter_voice_note/static/src/components/audio_to_text/audio_to_text.js
@@ -1,24 +1,21 @@
-/** @odoo-module **/
+import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { getDefaultConfig } from "@web/views/view";
+import { Component, useSubEnv } from "@odoo/owl";
+import { VoiceRecorder } from "./voice_recorder"; // Asegúrate de importar tu componente
 
-  import { registry } from "@web/core/registry";
-  import { Layout } from "@web/search/layout";
-  import { getDefaultConfig } from "@web/views/view";
-
-  import { Component, useSubEnv } from "@odoo/owl";
-
-  export class Audio_to_text extends Component {
+export class Audio_to_text extends Component {
     static template = "chatter_voice_note.Audio_to_text";
-    static components = { Layout };
-    static props = {};
+    static components = { Layout, VoiceRecorder }; // Añade VoiceRecorder aquí
 
     setup() {
-          useSubEnv({
-              config: {
-                  ...getDefaultConfig(),
-                  ...this.env.config,
-              },
-          });
-      }
-  }
+        useSubEnv({
+            config: {
+                ...getDefaultConfig(),
+                ...this.env.config,
+            },
+        });
+    }
+}
 
-  registry.category("actions").add("chatter_voice_note.audio_to_text", Audio_to_text);
+registry.category("actions").add("chatter_voice_note.audio_to_text", Audio_to_text);
