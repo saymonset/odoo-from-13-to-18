0cc7631a6d3a3156f032b568dffaee3b7a882d8d
saymonset <saymon_set@hotmail.com>
2025-11-13 19:30:58 +0100

    constant test

--- arquitectura/odoo18/clientes/cliente1/extra-addons/extra/chatter_voice_note/static/src/components/audio_to_text/constants.js
+++ arquitectura/odoo18/clientes/cliente1/extra-addons/extra/chatter_voice_note/static/src/components/audio_to_text/constants.js
@@ -1,9 +1,9 @@
 /** @odoo-module **/
 //TEST
- // export const N8N_WEBHOOK_URL = "https://n8n.jumpjibe.com/webhook/test-audios";
+  export const N8N_WEBHOOK_URL = "https://n8n.jumpjibe.com/webhook/test-audios";
 
   //#  PRODUCCION
-export const N8N_WEBHOOK_URL = "https://n8n.jumpjibe.com/webhook/audios";
+//export const N8N_WEBHOOK_URL = "https://n8n.jumpjibe.com/webhook/audios";
 
 // CONFIGURACIÓN MÍNIMA Y COMPATIBLE
 export const AUDIO_CONSTRAINTS = {
